609ce8f555539ef9b542ec407cce4bdb1982faaf
prakasha08 <prakash2292004@gmail.com>
2024-08-10 00:12:10 +0530

    Small Projects Updated

---
+++ Small Projects/Set_Timer/script.js
@@ -0,0 +1,9 @@
+let timeoutId;
+function startTimer(){
+timeoutId = setTimeout(()=>window.alert("Hello"), 3000);
+console.log("STARTED");
+}
+function clearTimer(){
+clearTimeout(timeoutId);
+console.log("CLEARED");
+}
