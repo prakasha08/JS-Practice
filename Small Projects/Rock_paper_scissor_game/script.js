c26c679921bfc4ca3befa9f6e82ba9f3b574d51a
prakasha08 <prakash2292004@gmail.com>
2024-08-13 23:06:59 +0530

    Small Projects Updated

---
+++ Small Projects/Rock_paper_scissor_game/script.js
@@ -0,0 +1,41 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay= document.getElementById("resultDisplay");
+const playerScoreDisplay= document.getElementById("playerScoreDisplay");
+const computerScoreDisplay= document.getElementById("computerscoreDisplay");
+let playerScore=0;
+let computerScore=0;
+function playGame (playerChoice) {
+const computerChoice = choices [Math.floor(Math.random() * 3)]; 
+let result = "";
+if (playerChoice === computerChoice){
+    result = "IT'S A TIE!";
+    }
+    else{
+    switch(playerChoice) {
+    case "rock":
+        result =(computerChoice === "scissors") ? "YOU WIN!":"YOU LOSE!";
+    case "paper":
+        result =(computerChoice === "rock") ? "YOU WIN!":"YOU LOSE!";
+    case "scissors":
+        result =(computerChoice === "paper") ? "YOU WIN!":"YOU LOSE!";
+    }
+}
+    resultDisplay.classList.remove("greenText", "redText");
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}` ;
+    resultDisplay.textContent = result;
+    switch(result) {
+        case "YOU WIN!":
+        resultDisplay.classList.add("greenText");
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        break;
+        case "YOU LOSE!":
+        resultDisplay.classList.add("redText");
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        break;
+    }
+}
